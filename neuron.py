7616c8f5eeb8f9067e143916124532e0a0bc1a81
Itzhak <itzhakshmueli838@gmail.com>
2023-11-02 00:31:25 +0200

    adding an another layer

--- neuron.py
+++ neuron.py
@@ -1,7 +1,4 @@
-"""
-this is a script that creates a 3 neuron layers with 4 inputs each neuron
 
-"""
 import numpy as np
 inputs = [[1, 2, 3, 2.5],
           [2.0, 5.0, -1.0, 2.0],
@@ -10,7 +7,15 @@ inputs = [[1, 2, 3, 2.5],
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
-biases = [2, 3, 0.5]
 
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
+weights2 = [[0.2, 0.8, -0.5],
+           [0.5, -0.91, 0.26],
+           [-0.26, -0.27, 0.17]]
+
+biases = [2, 3, 0.5]
+biases2 = [2, 3, 0.5]
+
+output1 = np.dot(inputs, np.array(weights).T) + biases
+
+output2 = np.dot(output1, np.array(weights2).T) + biases2
+print(output1,"\n",output2)
