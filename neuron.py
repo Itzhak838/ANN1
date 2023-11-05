9f8f1cb06cf917cd0322636a48fc997d1a19a128
Itzhak <itzhakshmueli838@gmail.com>
2023-11-05 15:50:53 +0200

    class calculate an output layer by input layer

--- neuron.py
+++ neuron.py
@@ -1,21 +1,27 @@
-
+"""
+This is a simple neuron class to initialize and calculate a neuron a layer
+"""
 import numpy as np
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+np.random.seed(0)  # seed for random number generator
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]  # input data to neural network
 
-weights2 = [[0.2, 0.8, -0.5],
-           [0.5, -0.91, 0.26],
-           [-0.26, -0.27, 0.17]]
 
-biases = [2, 3, 0.5]
-biases2 = [2, 3, 0.5]
+# initialize weights - random values -1 to 1 ranged to avoid nuber explosion
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-output1 = np.dot(inputs, np.array(weights).T) + biases
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-output2 = np.dot(output1, np.array(weights2).T) + biases2
-print(output1,"\n",output2)
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
