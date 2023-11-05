2f9490004eecf91aece7eff034f7d2354c271266
Itzhak <itzhakshmueli838@gmail.com>
2023-11-05 16:30:41 +0200

    adding a class for ReLU activation and an activation functions file

--- neuron.py
+++ neuron.py
@@ -12,6 +12,7 @@ X = [[1, 2, 3, 2.5],
 # initialize weights - random values -1 to 1 ranged to avoid nuber explosion
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
+        self.output = None
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
@@ -19,9 +20,21 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
+class Activation_ReLU:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
 layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+# layer2 = Layer_Dense(5, 2)
 layer1.forward(X)
-print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+# print(layer1.output)
+# layer2.forward(layer1.output)
+# print(layer2.output)
+
+activation1 = Activation_ReLU()
+activation1.forward(layer1.output)
+print(activation1.output)
